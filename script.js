1276ce594520c71b5aa27d0f84e5ed096455d3cd
Mateus Cavalcanti <mateusguilherme0201@gmail.com>
2025-07-21 15:53:20 -0300

    Add JS

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ class MobileNavBar {
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
+        
         this.handleClick = this.handleClick.bind();
 
     }
