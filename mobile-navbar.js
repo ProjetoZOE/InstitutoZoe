a20992bab014c7c08c0b0f0495b399ffbff55987
Mateus Cavalcanti <mateusguilherme0201@gmail.com>
2025-07-21 16:50:36 -0300

    NaBar

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -5,15 +5,27 @@ class MobileNavBar {
         this.navList = document.querySelector(navList);
         this.navLinks = document.querySelectorAll(navLinks);
         this.activeClass = "active";
-        
-        this.handleClick = this.handleClick.bind();
+
+        this.handleClick = this.handleClick.bind(this);
 
     }
 
 
-    handleClick(){
-        console.log(this);
+    animateLinks() {
+        this.navLinks.forEach((link, index) => {          
+            link.style.animation
+                ? (link.style.animation = "")
+                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
+
+
+
+    handleClick() {
+
         this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);       
+        this.animateLinks();
     }
 
 
@@ -37,7 +49,7 @@ class MobileNavBar {
 const mobileNavBar = new MobileNavBar(
     ".mobile-menu",
     ".nav-list",
-    "nav-list li",
+    ".nav-list li",
 )
 
 mobileNavBar.init();
