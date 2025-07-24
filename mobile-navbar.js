7f643836eb7232552d6a8c29cfb81e6af2af6fd9
Mateus Cavalcanti <mateusguilherme0201@gmail.com>
2025-07-24 18:41:07 -0300

    Protótipo final

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,56 +1,56 @@
 class MobileNavBar {
 
-    constructor(mobileMenu, navList, navLinks) {
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
 
+  }
+
+
+  animateLinks() {
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+    });
+  }
+
+
+
+  handleClick() {
+
+    this.navList.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
+
+
+
+
+  addClickEvent() {
+
+    this.mobileMenu.addEventListener("click", this.handleClick);
+
+  }
+
+  init() {
+
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
-
-
-    animateLinks() {
-        this.navLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
-        });
-    }
-
-
-
-    handleClick() {
-
-        this.navList.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
-
-
-
-
-    addClickEvent() {
-
-        this.mobileMenu.addEventListener("click", this.handleClick);
-
-    }
-
-    init() {
-
-        if (this.mobileMenu) {
-            this.addClickEvent();
-        }
-        return this;
-    }
+    return this;
+  }
 }
 
 const mobileNavBar = new MobileNavBar(
-    ".mobile-menu",
-    ".nave-list",
-    ".nave-list li",
-    "social-icons",
+  ".mobile-menu",
+  ".nave-list",
+  ".nave-list li",
+  "social-icons",
 )
 
 mobileNavBar.init();
@@ -60,23 +60,23 @@ mobileNavBar.init();
 
 
 
- const scrollBtn = document.getElementById('scrollToTopBtn');
+const scrollBtn = document.getElementById('scrollToTopBtn');
 
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) {
-      scrollBtn.classList.add('show');
-    } else {
-      scrollBtn.classList.remove('show');
-    }
-  });
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
 
-  scrollBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+scrollBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
   });
+});
 
 
 
