236300094213ad149ddb7bcd81eab7f327b4ca01
SamuelGilvan7 <sankiragilvan@gmail.com>
2025-10-03 23:32:00 -0300

    adicionar instruções para equipe sobre menu e footer
    - Inseridos comentarios nos arquivos includes/nave.php e includes/footer.php
      explicando onde editar menu e rodape.
    - Criado guia interno para a equipe sobre boas praticas de alteracao.
    - Evita confusoes entre arquivos principais e componentes incluidos.
    - Recomendado sempre fazer backup ou commit antes de alteracoes.

--- index-ativ.php
+++ index-ativ.php
@@ -62,61 +62,7 @@
 <body>
 
     <header>
-
-        <nav>
-
-            <a class="logo" href="index.html" style="display: flex;align-items: center;"> <img src="img/logo.png"
-                    alt="Logo Instituto Zoe">
-
-            </a>
-
-
-
-
-
-            <ul class="nave-list">
-
-                <li><a href="index.html">Início</a></li>
-                <li><a href="index-camp.html">Campanhas</a></li>
-                <li>
-                    <button class="Drop btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"
-                        style="font-size: 16px;">
-                        Serviços
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="index-ativ.html" style="font-size: 16px;">Atividades</a></li>
-                        <li><a class="dropdown-item" href="index-saude.html" style="font-size: 16px;">Saúde</a>
-                        </li>
-                    </ul>
-                </li>
-                <li><a href="index-agend.html">Agendamento</a></li>
-                <li><a href="index-apoiador.html">Seja Apoiador(a)</a></li>
-            </ul>
-
-
-
-            <div class="social-icons nave-list">
-                <a href="https://www.instagram.com/instituicao.zoe/" target="_blank"><i class="bi bi-instagram"
-                        style="font-size: 3vh;"></i></a>
-                <a href="https://www.tiktok.com/@elesabracam?_t=ZM-8yUYqBd8iqW&_r=1" target="_blank"><i
-                        class="bi bi-tiktok" style="font-size: 3vh;"></i></a>
-                <a href="https://www.youtube.com/channel/UC7ONgI1ulSOE8iYjwWE3Kww" target="_blank"><i
-                        class="bi bi-youtube" style="font-size: 4vh;"></i></a>
-                <a href="https://wa.me/5581973410768" target="_blank"><i class="bi bi-whatsapp"
-                        style="font-size: 3vh;"></i></a>
-            </div>
-
-
-
-            <div class="mobile-menu">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-
-            </div>
-
-        </nav>
-
+        <?php include 'menu.php'; ?>
     </header>
 
     <main style="padding: 0; overflow-x: hidden;">
@@ -237,51 +183,7 @@
     </main>
 
 
-    <footer class="footer text-center text-lg-start">
-        <div class="container p-4">
-            <div class="row align-items-center text-center text-md-start">
-                <div class="col-md-4 mb-3 mb-md-0">
-                    <p class="mb-0">&copy; 2025 - Instituto Zoe - Todos os direitos reservados <br><br>
-                        Local: Rua Arezzo N°290 <br>
-                        Horário de funcionamento: 07:00 às 17:00 <br>(07:00 às 19:00 nas Quartas-feiras) <br><br>
-                        Telefone para contato: (81)97341-0768
-
-                    </p>
-                </div>
-                <div class="col-md-4 mb-3 mb-md-0 d-flex justify-content-center">
-                    <a class="logo d-flex align-items-center gap-2 text-decoration-none" href="#">
-                        <img src="img/logo.png" alt="" style="height: 21vh;">
-                    </a>
-                </div>
-                <div class="col-md-4 mb-3 mb-md-0 d-flex justify-content-md-end justify-content-center gap-3">
-
-                    <a href="https://www.instagram.com/instituicao.zoe/" target="_blank"
-                        class="text-decoration-none social-link"><i class="bi bi-instagram fs-4"
-                            style="color: rgb(251, 251, 251);"></i></a>
-
-                    <a href="https://www.tiktok.com/@elesabracam?_t=ZM-8yUYqBd8iqW&_r=1" target="_blank"
-                        class="text-decoration-none social-link"><i class="bi bi-tiktok fs-4"
-                            style="color: rgb(251, 251, 251);"></i></a>
-
-                    <a href="https://www.youtube.com/channel/UC7ONgI1ulSOE8iYjwWE3Kww" target="_blank"
-                        class="text-decoration-none social-link"><i class="bi bi-youtube fs-4"
-                            style="color: rgb(251, 251, 251);"></i></a>
-
-                    <a href="https://wa.me/5581973410768" target="_blank" class="text-decoration-none social-link"><i
-                            class="bi bi-whatsapp fs-4" style="color: rgb(251, 251, 251);"></i></a>
-
-                </div>
-                <div class="social-icons mt-3">
-                    <a href="https://facebook.com"><i class="bi bi-facebook"></i></a>
-                    <a href="fale_conosco.php" target="_blank"><i class="bi bi-youtube"></i></a>
-                </div>
-                <p class="mt-2"><a href="fale_conosco.php">Fale Conosco</a></p>
-            </div>
-        </div>
-        <a href="#" class="scroll-to-top" id="scrollToTopBtn">
-            <i class="bi bi-arrow-up-circle-fill"></i>
-        </a>
-    </footer>
+    <?php include 'footer.php'; ?>
 
 
 
